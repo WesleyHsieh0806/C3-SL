16ad1c3e3375df37c9ad4f8157b81a28db55905a
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-03 09:38:14 +0800

    Training Script

--- CIFAR10/split_compress_recon_grad/train.sh
+++ CIFAR10/split_compress_recon_grad/train.sh
@@ -2,4 +2,4 @@ BATCH=64
 EPOCH=70
 Lambda=0.0005
 DUMP_PATH='./log/Lambda_5e-4_Batch64'
-python train.py --batch ${batch size} --epoch ${epoch} --Lambda 0.0005 --dump_path ./log
+python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path ./log
