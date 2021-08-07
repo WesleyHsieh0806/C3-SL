ecaa3113805c8a599f9685b2569151c6bbe47480
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-07 05:12:00 +0000

    MSE training script

--- CIFAR10/split_compress_recon/train_MSE.sh
+++ CIFAR10/split_compress_recon/train_MSE.sh
@@ -2,4 +2,4 @@ Dump_path='./log/Lambda_5e-4_Log'
 BATCH=64
 EPOCH=70
 LAMBDA=0.0005
-python train.py --dump_path $Dump_path --batch $BATCH --epoch $EPOCH --Lambda $LAMBDA
+python train_MSE.py --dump_path $Dump_path --batch $BATCH --epoch $EPOCH --Lambda $LAMBDA
