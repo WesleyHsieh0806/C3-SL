344e6306d868aca9d20deacab75d03ccd2425811
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-12 11:03:55 +0800

    no giving up

--- CIFAR10/split_compress_ECC/train.sh
+++ CIFAR10/split_compress_ECC/train.sh
@@ -1,5 +1,5 @@
 BATCH=64
 EPOCH=70
 Lambda=0.0005
-DUMP_PATH='./log/wo_engrad/Lambda_5e-4_Batch64'
+DUMP_PATH='./log/Lambda_5e-4_Batch64'
 python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH
