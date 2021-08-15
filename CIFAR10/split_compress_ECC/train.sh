ddf5d5e498f8d0599a1551c4971e1dbff9d12557
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-16 01:34:19 +0800

    Test for large reconstruction lambda

--- CIFAR10/split_compress_ECC/train.sh
+++ CIFAR10/split_compress_ECC/train.sh
@@ -1,5 +1,5 @@
+DUMP_PATH='./log/Lambda_5e-3_Batch64'
 BATCH=64
 EPOCH=70
-Lambda=0.0
-DUMP_PATH='./log/Lambda_0_Batch64'
+Lambda=0.005
 python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH
