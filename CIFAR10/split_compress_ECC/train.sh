c50a881d2dec19f4461e3b15b7246ecdb3cdc382
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-16 03:54:54 +0000

    Large Lambda

--- CIFAR10/split_compress_ECC/train.sh
+++ CIFAR10/split_compress_ECC/train.sh
@@ -1,5 +1,5 @@
-DUMP_PATH='./log/Lambda_5e-3_Batch64'
+DUMP_PATH='./log/Lambda_5e-2_Batch64'
 BATCH=64
 EPOCH=70
-Lambda=0.005
+Lambda=0.05
 python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH
