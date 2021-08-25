eadde3964416b763a1d461e32f89fe3da9dc0db3
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-25 04:13:02 +0000

    colllectz

--- CIFAR10/split_compress_ECC/collect_z.sh
+++ CIFAR10/split_compress_ECC/collect_z.sh
@@ -1,6 +1,6 @@
-DUMP_PATH='./log/Lambda_0_Batch64'
+DUMP_PATH='./log/Lambda_0_Batch64_ep100'
 BATCH=64
 EPOCH=70
 Lambda=0.0
-Restore_path='./log/Lambda_0_Batch64'
+Restore_path='./log/Lambda_0_Batch64_ep100'
 python collect_z.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH --restore_path $Restore_path
