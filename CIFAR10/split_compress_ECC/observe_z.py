4de56efa033e124fc02d54bcea0375ea4eeeab86
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-26 11:34:51 +0800

    observation

--- CIFAR10/split_compress_ECC/observe_z.py
+++ CIFAR10/split_compress_ECC/observe_z.py
@@ -9,3 +9,5 @@ file_path = os.path.join(dump_path, "features.pkl")
 with open(file_path, "rb") as f:
     stored_dict = pickle.load(f)
 z, recover_z = stored_dict["z"], stored_dict["recover_z"]
+print(z.shape)
+print(recover_z.shape)
