4dc91d6d3f9b15acf302dc62750e32cc3aef89ea
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-26 11:34:13 +0800

    observation

---
+++ CIFAR10/split_compress_ECC/observe_z.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import pickle
+dump_path = './log/Lambda_0_Batch64_ep100'
+file_path = os.path.join(dump_path, "features.pkl")
+
+with open(file_path, "rb") as f:
+    stored_dict = pickle.load(f)
+z, recover_z = stored_dict["z"], stored_dict["recover_z"]
