4f2756827b77d1bfa55ec40f86f349cd924fbc4b
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-26 11:39:57 +0800

    observation

--- CIFAR10/split_compress_ECC/observe_z.py
+++ CIFAR10/split_compress_ECC/observe_z.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import pickle
+from utils import
+
 dump_path = './log/Lambda_0_Batch64_ep100'
 file_path = os.path.join(dump_path, "features.pkl")
 
@@ -11,3 +13,17 @@ with open(file_path, "rb") as f:
 z, recover_z = stored_dict["z"], stored_dict["recover_z"]
 print(z.shape)
 print(recover_z.shape)
+
+# check for cross-correlation
+
+
+def CrossCorrelation(z):
+    z = z - z.mean(0)
+    z = z/torch.norm(z, dim=0)
+    return z.T @ z
+
+
+CC_z = CrossCorrelation(z)
+CC_recover_z = CrossCorrelation(recover_z)
+print("MSR between Cross Correlation matrix:{:.5f}".format(
+    torch.mean((CC_z-CC_recover_z)**2)))
