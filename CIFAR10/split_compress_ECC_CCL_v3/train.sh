3791d3ed490d8a799f9b2a519fb622dc546c9573
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-08-29 17:02:24 +0800

    Modify the arguments

--- CIFAR10/split_compress_ECC_CCL_v3/train.sh
+++ CIFAR10/split_compress_ECC_CCL_v3/train.sh
@@ -2,4 +2,5 @@ DUMP_PATH='./log/Lambda_1e-1_Batch64_Ep100'
 BATCH=64
 EPOCH=100
 Lambda=0.1
-python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH
+beta=0.005
+python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH --beta $beta
