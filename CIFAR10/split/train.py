de834e0d063e039424a3f99f06b2cc6d3b33b1fb
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-09-03 00:18:49 +0800

    new augmentation

--- CIFAR10/split/train.py
+++ CIFAR10/split/train.py
@@ -39,15 +39,15 @@ print("Size of testing labels:{}".format(test_labels.shape))
 ********************************************
 '''
 train_transform = transforms.Compose([
-    transforms.ToPILImage(),
-    # transforms.Resize([128, 128]),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 test_transform = transforms.Compose([
-    transforms.ToPILImage(),
-    # transforms.Resize([128, 128]),
     transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 ''' 
 * Argument Parser
