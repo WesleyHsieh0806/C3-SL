ac7840fbe79c0f8d013675b147ee4efd8d9d9404
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-09-05 01:45:52 +0800

    Modify lambda scheduler

--- CIFAR10/split_compress_ECC_rec_v2/train.sh
+++
@@ -1,6 +0,0 @@
-DUMP_PATH='./log/Lambda_0_Batch64_ep100'
-BATCH=64
-EPOCH=100
-Lambda=0.0
-ARCH="resnet50"
-python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH --arch $ARCH
