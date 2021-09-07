38714601b560e34f1ecdf5e7f2492e0c45e09356
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-09-07 17:11:29 +0000

    log

--- CIFAR10/split_compress_ECC/train.sh
+++ CIFAR10/split_compress_ECC/train.sh
@@ -1,6 +1,6 @@
-DUMP_PATH='./Resnet50_log/Scheduler/Lambda_5_Batch128_ep100'
-BATCH=128
+DUMP_PATH='./Resnet50_log/Scheduler/Lambda_0_Batch256_ep100'
+BATCH=256
 EPOCH=100
-Lambda=5.0
+Lambda=0.0
 ARCH="resnet50"
 python train.py --batch $BATCH --epoch $EPOCH --Lambda $Lambda --dump_path $DUMP_PATH --arch $ARCH
