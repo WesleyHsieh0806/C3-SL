5c0abdc26d9a625b139fad427b152ce9a503fd23
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2021-09-15 18:36:04 +0800

    ECC

--- CIFAR10/split_compress_ECC_v2/train.sh
+++ CIFAR10/split_compress_ECC_v2/train.sh
@@ -7,7 +7,6 @@ DUMP_PATH="./Resnet50_log/Middle/Compression${Batch_Compression_Ratio}_Batch${BA
 python train.py \
 --batch $BATCH \
 --epoch $EPOCH \
---Lambda $Lambda \
 --dump_path $DUMP_PATH \
 --arch $ARCH \
 --split $SPLIT \
