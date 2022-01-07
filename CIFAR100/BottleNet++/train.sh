539efdfa3c5a2a40c685448d51bfc6084fdbbe15
frank chuang <b06901053@ntu.edu.tw>
2022-01-07 18:42:57 +0000

    BCR4

--- CIFAR100/BottleNet++/train.sh
+++ CIFAR100/BottleNet++/train.sh
@@ -1,7 +1,7 @@
 BATCH=64
-EPOCH=200
+EPOCH=150
 ARCH='resnet50'
-SPLIT="linear"
-Compression_ratio=64
-Dump_path="./Resnet50_log/Linear/B${BATCH}_Compress${Compression_ratio}_Ep${EPOCH}"
-python train.py --dump_path $Dump_path --batch $BATCH --epoch $EPOCH --arch $ARCH --split $SPLIT --compression_ratio $Compression_ratio
+SPLIT="middle-2"
+Compression_ratio=4
+Dump_path="./Resnet50_log/${SPLIT}/B${BATCH}_Compress${Compression_ratio}_Ep${EPOCH}"
+python3 train.py --dump_path $Dump_path --batch $BATCH --epoch $EPOCH --arch $ARCH --split $SPLIT --compression_ratio $Compression_ratio
