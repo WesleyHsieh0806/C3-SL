9b347e1ad0d5890725c64587d6856f1f47fbc832
WesleyHsieh0806 <b06901053@ntu.edu.tw>
2022-01-09 11:11:54 +0000

    WTF

--- CIFAR10/BottleNet++/train.sh
+++
@@ -1,7 +0,0 @@
-BATCH=64
-EPOCH=100
-ARCH='vgg16'
-SPLIT="middle-2"
-Compression_ratio=4
-Dump_path="./${ARCH}_log/${SPLIT}/B${BATCH}_Compress${Compression_ratio}_Ep${EPOCH}"
-python3 train.py --dump_path $Dump_path --batch $BATCH --epoch $EPOCH --arch $ARCH --split $SPLIT --compression_ratio $Compression_ratio
