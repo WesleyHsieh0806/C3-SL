5a0f7bca29d4f942d2c148d190e213d181b15c5c
Cheng-Yen Hsieh <55971907+WesleyHsieh0806@users.noreply.github.com>
2022-08-06 16:54:29 +0800

    training script

---
+++ CIFAR100/C3-SL/train.sh
@@ -0,0 +1,13 @@
+BATCH=64
+EPOCH=150
+ARCH="resnet50"
+SPLIT="middle-2"
+Batch_Compression_Ratio=16
+DUMP_PATH="./Resnet50_log/${SPLIT}/Compression${Batch_Compression_Ratio}_Batch${BATCH}_ep${EPOCH}"
+python train.py \
+--batch $BATCH \
+--epoch $EPOCH \
+--dump_path $DUMP_PATH \
+--arch $ARCH \
+--split $SPLIT \
+--bcr $Batch_Compression_Ratio
